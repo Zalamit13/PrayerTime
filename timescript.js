883fe7c812cf47072af00ad7fdd4b879e845d506
Zalamit13 <115380255+Zalamit13@users.noreply.github.com>
2023-11-17 00:53:43 +0100

    Ok massive changes this time
    All the available states with their coordinates
    I added some styling so css update
    Two js scripts one for the drop menu button styling and the other for the time, i will add more to the time one to complete the websites functionality.

---
+++ timescript.js
@@ -0,0 +1,24 @@
+function updateDateTime() {
+    const currentDateElement = document.getElementById("currentDate");
+    const islamicDateElement = document.getElementById("islamicDate");
+    const currentTimeElement = document.getElementById("currentTime");
+
+    const now = new Date();
+
+    // Update current date
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    currentDateElement.textContent = now.toLocaleDateString('en-US', options);
+
+    // Update Islamic date (you might need a library or API for accurate conversion)
+    islamicDateElement.textContent = "Rabi' al-Awwal 7, 1445";
+
+    // Update current time
+    const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+    currentTimeElement.textContent = now.toLocaleTimeString('en-US', timeOptions);
+}
+
+// Update every second
+setInterval(updateDateTime, 1000);
+
+// Initial update
+updateDateTime();
